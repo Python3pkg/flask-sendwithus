359916cb961e405611e866e41c04dd223213a7c9
Jacob Magnusson <m@jacobian.se>
2015-10-01 15:02:48 +0200

    setup.py fixes

--- setup.py
+++ setup.py
@@ -1,3 +1,39 @@
+"""
+Flask-Sendwithus
+================
+
+About
+-----
+
+Forwards-compatible Flask extension to interact with the `sendwithus <https://www.sendwithus.com/>`_ API.
+
+Installation
+------------
+
+    pip install Flask-Sendwithus
+
+Documentation
+-------------
+
+Uses the standard extension pattern. Example::
+
+    >>> from flask import Flask
+    >>> from flask_sendwithus import Sendwithus
+
+    >>> app = Flask(__name__)
+    >>> app.config['SENDWITHUS_API_KEY'] = 'YOUR-API-KEY'
+    >>> sendwithus = Sendwithus()
+    >>> sendwithus.init_app(app)
+    >>> r = sendwithus.send(
+        email_id='YOUR-EMAIL-ID',
+        recipient={'address': 'us@sendwithus.com'})
+    >>> print(r.status_code)
+    200
+    )
+
+See `the official python client's documentation <https://github.com/sendwithus/sendwithus_python)>`_ for further info on what methods are available. All methods found on the `sendwithus.api` instance is proxied on the Flask-Sendwithus's instance.
+
+"""
 from setuptools import setup
 
 setup(
@@ -5,8 +41,11 @@ setup(
     version='1.0',
     author="Jacob Magnusson",
     author_email="m@jacobian.se",
+    url='https://github.com/jmagnusson/Flask-Resize',
+    platforms='any',
     license="BSD",
     description="Forwards-compatible Flask extension to interact with the sendwithus API",
+    long_description=__doc__,
     packages=['flask_sendwithus'],
     install_requires=['flask>=0.8', 'sendwithus'],
     classifiers=[
